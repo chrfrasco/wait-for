e82a8032e49890226f08af2591fe1ca6a1e43fd7
Christian Scott <christianfscott@gmail.com>
2018-07-11 18:55:22 +1200

    add failing test case

--- url_waiter_test.go
+++ url_waiter_test.go
@@ -1,22 +1,68 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 )
 
-func makeMockGet(fn func(url string)) func(url string) (*http.Response, error) {
-	return func(url string) (*http.Response, error) {
-		fn(url)
-		return &http.Response{}, nil
+func TestHappyPath(t *testing.T) {
+	calls := []string{}
+
+	mockGet := func(url string) (*http.Response, error) {
+		calls = append(calls, url)
+		return &http.Response{
+			StatusCode: 200,
+			Status:     "200 OK",
+		}, nil
+	}
+
+	urlWaiter := newURLWaiter(mockGet)
+	urls := []string{"http://google.com"}
+	urlWaiter.waitForURLs(urls)
+
+	if !sliceEqual(calls, urls) {
+		t.Errorf("expected: %v\n got: %v", urls, calls)
 	}
 }
 
-func TestURLWaiterBasic(t *testing.T) {
+func TestBailsAfterError(t *testing.T) {
 	calls := []string{}
-	mockGet := makeMockGet(func(url string) {
+
+	mockGet := func(url string) (*http.Response, error) {
 		calls = append(calls, url)
-	})
+		return nil, errors.New("")
+	}
+
 	urlWaiter := newURLWaiter(mockGet)
-	urlWaiter.waitForURLs([]string{"http://google.com"})
+	urls := []string{"http://google.com"}
+	urlWaiter.waitForURLs(urls)
+
+	expected := []string{}
+
+	if !sliceEqual(expected, calls) {
+		t.Errorf("expected: %v\n got: %v", urls, calls)
+	}
+}
+
+func sliceEqual(a []string, b []string) bool {
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
 }
